a93797a2b7d0875ca5b553e3fa596ecd6aebaa7d
David Zbarsky <dzbarsky@gmail.com>
2014-03-28 21:10:08 -0400

    add dialer

--- src/com/example/meditation/MainActivity.java
+++ src/com/example/meditation/MainActivity.java
@@ -1,18 +1,70 @@
 package com.example.meditation;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
 
 public class MainActivity extends Activity {
 
+	// monitor phone call states
+		private class PhoneCallListener extends PhoneStateListener {
+
+			String TAG = "LOGGING PHONE CALL";
+
+			private boolean phoneCalling = false;
+
+			@Override
+			public void onCallStateChanged(int state, String incomingNumber) {
+
+				if (TelephonyManager.CALL_STATE_RINGING == state) {
+					// phone ringing
+					Log.i(TAG, "RINGING, number: " + incomingNumber);
+				}
+
+				if (TelephonyManager.CALL_STATE_OFFHOOK == state) {
+					// active
+					Log.i(TAG, "OFFHOOK");
+
+					phoneCalling = true;
+				}
+
+				// When the call ends launch the main activity again
+				if (TelephonyManager.CALL_STATE_IDLE == state) {
+
+					Log.i(TAG, "IDLE");
+
+					if (phoneCalling) {
+
+						Log.i(TAG, "restart app");
+
+						// restart app
+						Intent i = getBaseContext().getPackageManager()
+								.getLaunchIntentForPackage(
+										getBaseContext().getPackageName());
+
+						i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+						startActivity(i);
+
+						phoneCalling = false;
+					}
+
+				}
+			}
+		}
+		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,6 +74,26 @@ public class MainActivity extends Activity {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+		
+		Button button = (Button) findViewById(R.id.button);
+
+		PhoneCallListener phoneCallListener = new PhoneCallListener();
+		TelephonyManager telManager = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+		telManager.listen(phoneCallListener, PhoneStateListener.LISTEN_CALL_STATE);
+
+		// add button listener
+		button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+
+				Intent phoneCallIntent = new Intent(Intent.ACTION_CALL);
+				phoneCallIntent.setData(Uri.parse("tel:123456"));
+				startActivity(phoneCallIntent);
+
+			}
+
+		});
 	}
 
 	@Override
