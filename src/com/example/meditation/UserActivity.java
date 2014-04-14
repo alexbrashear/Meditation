ce85dc1a489d71fb37faddcef8012b439b7ccf5b
tkoane <tkoane@gmail.com>
2014-04-13 21:50:37 -0400

    adding the buttons

--- src/com/example/meditation/UserActivity.java
+++ src/com/example/meditation/UserActivity.java
@@ -6,6 +6,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -18,6 +20,48 @@ public class UserActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
         ua = this;
+        //set pain button
+        final Button pain = (Button) findViewById(R.id.pain_button);
+        pain.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Log.e("question: ", "I am experiencing a pain when I do this.");
+				ParsePush push = new ParsePush();
+				push.setChannel("Question");
+				push.setMessage("I am experiencing a pain when I do this.");
+				push.sendInBackground();
+				Toast.makeText(UserActivity.this, "Question sent", Toast.LENGTH_LONG).show();
+			}
+		});
+        //set slow button
+        final Button slow = (Button) findViewById(R.id.slow_button);
+        slow.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Log.e("question: ", "Can you please slow down a little?");
+				ParsePush push = new ParsePush();
+				push.setChannel("Question");
+				push.setMessage("Can you please slow down a little?");
+				push.sendInBackground();
+				Toast.makeText(UserActivity.this, "Question sent", Toast.LENGTH_LONG).show();
+			}
+		});
+        //set repeat button
+        final Button rep = (Button) findViewById(R.id.repeat_button);
+        rep.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Log.e("question: ", "We have already talked about this last week.");
+				ParsePush push = new ParsePush();
+				push.setChannel("Question");
+				push.setMessage("We have already talked about this last week.");
+				push.sendInBackground();
+				Toast.makeText(UserActivity.this, "Question sent", Toast.LENGTH_LONG).show();
+			}
+		});
 	}
 
 	@Override
@@ -35,6 +79,18 @@ public class UserActivity extends Activity {
 		}
 		
 		return super.dispatchKeyEvent(event);
+	}
+	
+	public void sendToInstructor(View view) {
+		custom_q = (EditText) findViewById(R.id.custom_q);
+		Log.e("question: ", custom_q.getText().toString());
+		ParsePush push = new ParsePush();
+		push.setChannel("Question");
+		push.setMessage(custom_q.getText().toString());
+		push.sendInBackground();
+		Toast.makeText(UserActivity.this, custom_q.getText(), Toast.LENGTH_LONG).show();
+		custom_q.setText("");
+		
 	}
 	
 }
