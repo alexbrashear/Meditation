a4a2dfaa38cb5d6bb73d1e0e4df6730b1a3169d9
tkoane <tkoane@gmail.com>
2014-04-27 21:39:11 -0400

    fixed UserEnd

--- src/com/example/meditation/ViewResultsActivity.java
+++ src/com/example/meditation/ViewResultsActivity.java
@@ -8,7 +8,7 @@ public class ViewResultsActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.view_results);
+        setContentView(R.layout.user_end);
 	}
 
 }
